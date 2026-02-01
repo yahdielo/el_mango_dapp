ca36a5256d753b14dedc708b7fc016d38e950831
LAB <srcolon119@gmail.com>
2026-02-01 05:35:49 -0400

    backend setup

--- src/components/MobileSettingsBox.js
+++ src/components/MobileSettingsBox.js
@@ -1,9 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import { useAccount, useChainId, useDisconnect } from 'wagmi';
+import React from 'react';
+import { useAccount, useChainId } from 'wagmi';
 import { useNavigate } from 'react-router-dom';
 import MobileSwapHeader from './MobileSwapHeader';
 import BottomNavigation from './BottomNavigation';
-import WalletSettings from './settings/WalletSettings';
 import PreferencesSettings from './settings/PreferencesSettings';
 import SecuritySettings from './settings/SecuritySettings';
 import ReferralSettings from './settings/ReferralSettings';
@@ -13,11 +12,9 @@ import './css/SettingsMobile.css';
 const MobileSettingsBox = () => {
     const { address, isConnected } = useAccount();
     const chainId = useChainId();
-    const { disconnect } = useDisconnect();
     const navigate = useNavigate();
 
     const handleMenuClick = () => {
-        // Could navigate back or open menu
         navigate(-1);
     };
 
@@ -29,26 +26,14 @@ const MobileSettingsBox = () => {
             />
             
             <div className="mobile-settings-content">
-                {/* Wallet Settings */}
-                <WalletSettings 
-                    address={address}
-                    isConnected={isConnected}
-                    chainId={chainId}
-                    onDisconnect={disconnect}
-                />
-
-                {/* App Preferences */}
                 <PreferencesSettings />
 
-                {/* Security Settings */}
                 <SecuritySettings />
 
-                {/* Referral & Rewards */}
                 {isConnected && (
                     <ReferralSettings address={address} chainId={chainId} />
                 )}
 
-                {/* Chain Management */}
                 <ChainManagement chainId={chainId} />
             </div>
 
